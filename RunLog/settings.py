6e1d52b12cc49d6e244fc8786d8b5ebcffc00b59
Lauri Leskinen <laurileskinen@icloud.com>
2022-10-29 17:55:55 +0300

    Updated settings

--- RunLog/settings.py
+++ RunLog/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'RunDB',
         'USER': 'postgres',
-        'PASSWORD': 'Fawad4F8ka',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5433',
     }
